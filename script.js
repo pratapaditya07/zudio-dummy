10d7b1a2673fe3e9614f8b15916583b5adf2ad6e
pratapaditya07 <139495535+pratapaditya07@users.noreply.github.com>
2025-02-14 18:57:39 +0530

    g

--- script.js
+++ script.js
@@ -2,52 +2,14 @@ gsap.to("#pg1_video h1" , {
     fontSize: "2.5vw",
     top: "0.8vw",
     left: "0vw",
+    color: "#101000",
     scrollTrigger: {
         trigger: "#pg1_video h1",
         scroll: "#pg1_video",
-        start: "top 75.8%",
+        start: "top 75.9%",
         end: "top -15%",
         scrub: 1,
         pin: "#pg1_video"
     }
 })
 
-    /* gsap.registerPlugin(ScrollTrigger);
-
-    const tl = gsap.timeline({
-        scrollTrigger: {
-        trigger: "#pg1_video",
-        start: "top top",
-        end: "+=500px",
-        scrub: true,
-        pin: true
-        }
-    });
-    
-    tl.to("#brand_name", {
-        top: "10px", // Adjust as per your design
-        left: "10px", // Adjust as per your design
-        scale: 0.5, // Adjust to make the text smaller if needed
-        position: "absolute", // Ensure the positioning works as intended
-        ease: "power1.out"
-    });
-    
-    // Enable normal page scrolling after text is positioned
-    ScrollTrigger.create({
-        trigger: "#pg1_video",
-        start: "bottom top",
-        end: "bottom top+=1",
-        onEnter: () => {
-        gsap.to(".page-content", {
-            y: 0,
-            ease: "none"
-        });
-        },
-        onLeaveBack: () => {
-        gsap.to(".page-content", {
-            y: 0,
-            ease: "none"
-        });
-        }
-    }); */
-    
