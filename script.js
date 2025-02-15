1c930ea3eabb6b1f68f081cb839d140eed29071b
pratapaditya07 <139495535+pratapaditya07@users.noreply.github.com>
2025-02-15 12:09:06 +0530

    z

--- script.js
+++ script.js
@@ -13,3 +13,147 @@ gsap.to("#pg1_video h1" , {
     }
 })
 
+/* gsap.to("#page2 img" , {
+    height : "100%",
+    scrollTrigger:{
+        trigger: "#page2 img",
+        start: "top 75.9%",
+        end: "top -15%",
+        scrub: 1,
+        pin: "#page2"
+    }
+}) */
+
+/* gsap.to("#eng",{
+    display: "block",
+    scrollTrigger:{
+        trigger: "#eng",
+        scroll:"#ani",
+        markers:true,
+        start: "top 25%",
+        end: "top 20%",
+        scrub:1,
+        pin: "#page2"
+    }
+})
+
+gsap.to("#eng",{
+    display: "none",
+    scrollTrigger:{
+        trigger: "#eng",
+        scroll:"#ani",
+        markers:true,
+        start: "top 20%",
+        end: "top -100000%",
+        scrub:1,
+        pin: "#page2"
+    }
+})  */
+
+gsap.to("#urdu",{
+    display: "block",
+    color: "#e3e3ee",
+    scrollTrigger:{
+        trigger: "#urdu",
+        scroll:"#ani",
+        markers:true,
+        start: "top 25%",
+        end: "top 20%",
+        scrub:1,
+        pin: "#page2"
+    }
+})
+
+/* gsap.to("#urdu",{
+    display: "none",
+    scrollTrigger:{
+        trigger: "#urdu",
+        scroll:"#ani",
+        markers:true,
+        start: "top 15%",
+        end: "top -100000%",
+        scrub:1,
+        pin: "#page2"
+    }
+}) */
+
+/* gsap.to("#tamil",{
+    display: "block",
+    scrollTrigger:{
+        trigger: "#tamil",
+        scroll:"#ani",
+        markers:true,
+        start: "top 15%",
+        end: "top 10%",
+        scrub:1,
+        pin: "#page2"
+    }
+})
+
+gsap.to("#tamil",{
+    display: "none",
+    scrollTrigger:{
+        trigger: "#tamil",
+        scroll:"#ani",
+        markers:true,
+        start: "top 10%",
+        end: "top -100000%",
+        scrub:1,
+        pin: "#page2"
+    }
+})
+
+gsap.to("#hin",{
+    display: "block",
+    scrollTrigger:{
+        trigger: "#hin",
+        scroll:"#ani",
+        markers:true,
+        start: "top 10%",
+        end: "top 05%",
+        scrub:1,
+        pin: "#page2"
+    }
+})
+
+gsap.to("#hin",{
+    display: "none",
+    scrollTrigger:{
+        trigger: "#hin",
+        scroll:"#ani",
+        markers:true,
+        start: "top 05%",
+        end: "top -100000%",
+        scrub:1,
+        pin: "#page2"
+    }
+}) */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
