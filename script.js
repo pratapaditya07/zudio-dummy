76b5138a3bcd636ceffc4c9308d03a379f46f985
pratapaditya07 <139495535+pratapaditya07@users.noreply.github.com>
2025-02-26 11:46:23 +0530

    changed some way

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ gsap.from("#hero h1" , {
     fontSize : "20vw",
     scrollTrigger:{
         trigger: "#hero h1",
-        scroll:"main",
+        scroll:"#main",
         markers : true,
         start : "top 31",
         end : "top -150%",
         scrub : 2,
-        pin: "main"
+        pin : "#hero"
     }
 })
 
-gsap.to("#circle",{
-    height: " 100vh",
-    width: "100%",
-    borderRadius : "20px",
+gsap.to("#inn",{
+    height: " 95%",
+    width: "95%",
+    borderRadius : "15px",
     scrollTrigger:{
-        trigger: "#circle",
-        scroll: "main",
+        trigger: "#inn",
+        scroll: "#main",
         markers : true,
-        start : "top -45%",
-        end :"top -165%",
-        scrub: 2
+        start : "top 100%",
+        end :"top 10%",
+        scrub : 2
     }
 })
 
