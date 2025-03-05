5e98f0e183576aa8634dfcf7ffc18b4fe492f327
pratapaditya07 <139495535+pratapaditya07@users.noreply.github.com>
2025-03-06 00:47:15 +0530

    all day

--- script.js
+++ script.js
@@ -36,18 +36,42 @@ btn.addEventListener("click", () => {
         }
 });
 
-gsap.to("#page4 h1", {
-    transform: "translateX(-70%)",
-    scrollTrigger: {
-        trigger: "#page4",
-        scroll: "#page4",
-        start: "top 0", 
-        end: "top -200%",
-        scrub: 2,
-        pin: true
-    }
 
-})
+    gsap.to("#page3 img", {
+        height: "30vh",
+        width : "30vh",
+        scrollTrigger:{
+            trigger: "#page3",
+            scroll: "#main",
+            markers : true,
+            start : "top 0%",
+            end : "top -200%",
+            pin : "#page3",
+            scrub : 2
+        }
+    })
+
+    gsap.to("#page3l p",{
+        transform: "translateX(-25%)",
+        scrollTrigger:{
+            trigger : "#page3 p",
+            scroll : "#main",   
+            start : "top 0%",
+            end : "top -100%",
+            scrub : 2
+        }
+    })
+
+    gsap.to("#page3R p",{
+        transform: "translateX(25%)",
+        scrollTrigger:{
+            trigger : "#page3 p",
+            scroll : "#main",   
+            start : "top 0%",
+            end : "top -100%",
+            scrub : 2
+        }
+    })
 
 
 
@@ -60,12 +84,40 @@ gsap.to("#page4 h1", {
 
 
 
-/* gsap.from("#hero h1" , {
-    fontSize: "20.5vw",
-    scrollTrigger: {
-        trigger: "#hero h1",
-        scroll: "#hero",
-        scrub: 1,
-    }
-})
- */
+
+
+
+
+
+    gsap.to("#page4 h1", {
+        transform: "translateX(-70%)",
+        scrollTrigger: {
+            trigger: "#page4 h1",
+            scroll: "#main",
+            start: "top 9%", 
+            end: "top -200%",
+            scrub: 2,
+            pin: "#page4"
+        }
+    
+    })
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
